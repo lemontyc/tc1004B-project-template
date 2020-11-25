4c8d34e10aebcb87934eaf755fe341fce6841638
Luis Montoya <a01208334@itesm.mx>
2020-11-24 18:52:27 -0600

    Added plotting logic

--- js/gauge.js
+++ js/gauge.js
@@ -1,11 +1,17 @@
 var opts = {
-    angle: 0.1, // The span of the gauge arc
+    angle: 0.04, // The span of the gauge arc
     lineWidth: 0.3, // The line thickness
     radiusScale: 1, // Relative radius
     pointer: {
       length: 0.6, // // Relative to gauge radius
       strokeWidth: 0.035, // The thickness
       color: '#000000' // Fill color
+    },
+    staticLabels: {
+        font: "10px sans-serif",  // Specifies font
+        labels: [0, 100, 200, 300, 400, 500],  // Print labels at these values
+        color: "#000000",  // Optional: Label text color
+        fractionDigits: 0  // Optional: Numerical precision. 0=round off.
     },
     limitMax: false,     // If false, max value increases automatically if value > maxValue
     limitMin: false,     // If true, the min value of the gauge will be fixed
@@ -14,12 +20,17 @@ var opts = {
     strokeColor: '#E0E0E0',  // to see which ones work best for you
     generateGradient: true,
     highDpiSupport: true,     // High resolution support
+    percentColors: [[0.0, "#a9d70b" ], [200, "#f9c802"], [400, "#ff0000"]]   // Make color react according to the position of the needle
     
   };
 
   var target = document.getElementById('myGauge'); // your canvas element
   var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-  gauge.maxValue = 3000; // set max gauge value
+
+  gauge.maxValue = 500; // set max gauge value
   gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
-  gauge.animationSpeed = 32; // set animation speed (32 is default value)
-  gauge.set(1500); // set actual value
+  gauge.animationSpeed = 57; // set animation speed (32 is default value)
+  
+  
+
+  
--- js/myChart.js
+++ js/myChart.js
@@ -17,3 +17,82 @@ var chart = new Chart(ctx, {
     // Configuration options go here
     options: {}
 });
+
+// Function to add new data to a chart
+function addData(chart, label, data) 
+{
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => 
+    {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+// Plot all the data at the database
+$.ajax(
+    '../data/all.php',
+    {
+        success: function(data) {
+            var jsonData = JSON.parse(data);
+            var sensor1Data ;    // equivalent to sensor1Value
+            var sensor2Data ;    // equivalent to sensor2Value
+            var sensorTime ;    // converts timestamp to time (used as label)
+            for(row in jsonData){
+                // Extract sensor1Data
+                sensor1Data = jsonData[row]['sensor1Value'];
+                sensor2Data = jsonData[row]['sensor2Value'];
+                // Extract time from timestamp
+                sensorTime = new Date(jsonData[row]['timestamp']).toLocaleTimeString();
+                // Add data to chart
+                addData(myChart, sensorTime, sensor1Data);
+            }
+            gauge.set(sensor2Data); // set value of the gauge to the last value of sensor2Value
+        },
+        error: function() {
+          console.log('There was some error performing the AJAX call!');
+        }
+     }
+  );
+
+
+  // Every 0.5s check for new data
+  function fetchLastData(){
+    $.ajax(
+        '../data/last.php',
+        {
+            success: function(data) {
+                var jsonData = JSON.parse(data);
+                var sensor1Data = jsonData[0]['sensor1Value']; 
+                var sensor2Data = jsonData[0]['sensor2Value']; 
+                var sensorTime = new Date(jsonData[0]['timestamp']).toLocaleTimeString();   
+                /* 
+                Use the last time the sensor was updated, and compare that time with
+                last record time. If different, update table.
+    
+                This technique is for demonstration purposes. A better way, should be 
+                add another field at the database and update it when data was added to chart.
+                */
+                if(myChart.data.labels[myChart.data.labels.length - 1] === sensorTime)
+                {
+                    // Do nothing
+                    console.log('No new data');
+                }
+                else
+                {
+                    // Add new record to chart
+                    addData(myChart, sensorTime, sensor1Data);
+                    gauge.set(sensor2Data); // set actual value
+                }
+    
+            },
+            error: function() {
+              console.log('There was some error performing the AJAX call!');
+            }
+        }
+    );
+  }
+
+  setInterval(function(){ 
+    fetchLastData(); 
+}, 500);
